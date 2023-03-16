1dca7c2b19f23e2671b0b86efb9c99214e6147c5
rom201 <kovalchukr1104@gmail.com>
2023-03-15 20:49:49 -0400

    added html files and controller

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -18,23 +18,23 @@ public class BankSimulationAppApplication {
 //        SpringApplication.run(BankSimulationAppApplication.class, args);
         ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
 
-        AccountService accountService = container.getBean(AccountService.class);
-        TransactionService transactionService = container.getBean(TransactionService.class);
-
-        //create 2 accounts
-        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.CHECKING, 1L);
-        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.CHECKING, 2L);
+//        AccountService accountService = container.getBean(AccountService.class);
+//        TransactionService transactionService = container.getBean(TransactionService.class);
+//
+//        //create 2 accounts
+//        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.CHECKING, 1L);
+//        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.CHECKING, 2L);
 //        Account receiver2 = null;
 
 
-        accountService.listAllAccount().forEach(System.out::println);
-        transactionService.makeTransfer(sender, receiver, new BigDecimal(40),new Date(), "Transaction 1");
-
-
-        System.out.println(transactionService.findAllTransaction().get(0));
-
-        accountService.listAllAccount().forEach(System.out::println);
-        transactionService.makeTransfer(sender, receiver,new BigDecimal(40),new Date(), "Transaction 2");
+//        accountService.listAllAccount().forEach(System.out::println);
+//        transactionService.makeTransfer(sender, receiver, new BigDecimal(40),new Date(), "Transaction 1");
+//
+//
+//        System.out.println(transactionService.findAllTransaction().get(0));
+//
+//        accountService.listAllAccount().forEach(System.out::println);
+//        transactionService.makeTransfer(sender, receiver,new BigDecimal(40),new Date(), "Transaction 2");
 
 
 
---
+++ src/main/java/com/cydeo/controller/AccountController.java
@@ -0,0 +1,37 @@
+package com.cydeo.controller;
+
+import com.cydeo.model.Account;
+import com.cydeo.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/index")
+    public String getIndex(Model model){
+
+
+        model.addAttribute("accountList", accountService.listAllAccount());
+
+
+
+
+
+        return "account/index";
+    }
+
+
+
+}

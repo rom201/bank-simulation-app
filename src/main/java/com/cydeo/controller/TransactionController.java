62999559f4742769f5c800720b7e9da2c075736c
rom201 <kovalchukr1104@gmail.com>
2023-04-12 22:45:01 -0400

    validation for Transaction was completed

--- src/main/java/com/cydeo/controller/TransactionController.java
+++ src/main/java/com/cydeo/controller/TransactionController.java
@@ -1,16 +1,19 @@
 package com.cydeo.controller;
 
+import com.cydeo.emums.AccountType;
 import com.cydeo.model.Account;
 import com.cydeo.model.Transaction;
 import com.cydeo.service.AccountService;
 import com.cydeo.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -43,13 +46,18 @@ public class TransactionController {
     }
 
     @PostMapping("/transfer")
-    public String postMakeTransfer (@ModelAttribute ("transaction") Transaction transaction){
+    public String postMakeTransfer (@Valid @ModelAttribute ("transaction") Transaction transaction, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+
+            model.addAttribute("accounts", accountService.listAllAccount());
+            return "transaction/make-transfer";
+        }
 
         Account sender = accountService.retrieveById(transaction.getSender());
         Account receiver = accountService.retrieveById(transaction.getReceiver());
 
         transactionService.makeTransfer(sender,receiver, transaction.getAmount(), new Date(), transaction.getMessage());
-
         return "redirect:/make-transfer";
 
     }
--- src/main/java/com/cydeo/model/Transaction.java
+++ src/main/java/com/cydeo/model/Transaction.java
@@ -3,6 +3,7 @@ package com.cydeo.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
@@ -10,13 +11,22 @@ import java.util.UUID;
 @Data
 @Builder
 public class Transaction {
+
+    @NotNull
     private UUID sender;
+    @NotNull
     private UUID receiver;
 
-
+    @NotNull
+    @Positive
     private BigDecimal amount;
 
 
+    @Size(min = 2,max = 250)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
+//    @NotNull
+//    @NotBlank
+    @NotEmpty
     private String message;
 
     private Date creationDate;

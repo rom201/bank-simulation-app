94d0e5359abcfa58a67a5f301a97bc295ce7ff33
rom201 <kovalchukr1104@gmail.com>
2023-05-31 21:11:24 -0400

    security part 2

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
@@ -42,5 +44,12 @@ public class BankSimulationAppApplication {
     public ModelMapper mapper(){
         return new ModelMapper();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
 
 }
